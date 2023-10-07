b98cc73908b8b0e36f655d1c4efaa74c9d16d219
Arsenio02 <128214234+Arsenio02@users.noreply.github.com>
2023-10-07 12:00:01 +0600

    Add files via upload

---
+++ hw_8/script.js
@@ -0,0 +1,24 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+
+setInterval(nextSlide, 5000);
